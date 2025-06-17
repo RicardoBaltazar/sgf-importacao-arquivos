06c6e4fcb980edc5ebb95ffa99a178acc09636f2
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2025-06-16 21:58:10 -0300

    feat: Configure session expiration on browser close when Remember Me  is not used

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,11 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Auth\Events\Login;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Config;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +24,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Event::listen(Login::class, function ($event) {
+            if (!Auth::viaRemember()) {
+                Config::set('session.expire_on_close', true);
+                Session::put('_session_configured', true);
+                Session::migrate(true);
+            }
+        });
     }
 }
